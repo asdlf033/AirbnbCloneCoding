c49958570c30f7685c6c4387147c54393fc7fade
aitron-yej012 <aitron-yej012@aitronbiz.com>
2025-09-22 12:19:07 +0900

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by yej on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
